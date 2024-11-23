b10113c8bf4f8a9570af5cb211844f07c980b992
e.soloviev <e.soloviev@conundrum.ai>
2024-11-23 17:58:58 +0400

    Add menu and fix minor bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Route } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('@buygiftpresent/home').then(m => m.HomeComponent) },
+  { path: 'wishlist', loadComponent: () => import('@buygiftpresent/wishlist').then(m => m.WishlistComponent) },
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
