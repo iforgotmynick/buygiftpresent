10fae54649844fd0b44328e827e0383e1556f950
e.soloviev <e.soloviev@conundrum.ai>
2024-11-25 12:52:39 +0400

    Create separate navigation component

---
+++ libs/features/navigation-menu/src/lib/navigation-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'lib-navigation-menu',
+  standalone: true,
+  imports: [CommonModule, MenubarModule],
+  templateUrl: './navigation-menu.component.html',
+  styleUrl: './navigation-menu.component.css',
+})
+export class NavigationMenuComponent {
+  readonly menuItems: MenuItem[] = [
+    { label: 'Home', icon: 'pi pi-home', routerLink: ['/home'] },
+    { label: 'Wishlist', icon: 'pi pi-file-edit', routerLink: ['/wishlist'] },
+  ];
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { MenubarModule } from 'primeng/menubar';
+import {NavigationMenuComponent} from '@buygiftpresent/navigation-menu';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
-    MenubarModule, 
     RouterModule,
+    NavigationMenuComponent,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-
-  readonly menuItems = [
-    { label: 'Home', icon: 'pi pi-home', routerLink: ['/home'] },
-    { label: 'Wishlist', icon: 'pi pi-file-edit', routerLink: ['/wishlist'] },
-  ];
-
   title = 'buygiftpresent';
 }
